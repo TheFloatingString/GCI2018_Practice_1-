dde200de5bd9871c68a608ff21624edc6b7e59e1
Laurence Liang <laurencel2001@gmail.com>
2018-11-01 21:46:37 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Loop to print CGI is great
+for i in range(10):
+	print("CGI is great")
+
+# ask user for name
+user_name = input("What is your name? ")
+user_name_backwards = user_name[::-1]
+
+print(f"Hello {user_name}, please to meet you!")
+print(f"Did you know that your name backwards is {user_name_backwards}?")
